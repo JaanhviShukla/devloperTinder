1e7edee9000f9bd3b8099c2f78b3e23f6c4fa3ab
JaanhviShukla <jaanhvishukla11@gmail.com>
2025-09-05 00:57:52 +0530

    Fix: remove node_modules and update app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
-app.use("/info",(req,res)=>{
-  res.send("This webssite is created by JAANHVI SHUKLA")
-})
-app.use("/acknowledgement",(req,res)=>{
-  res.send("I would like to acknowledge my parents and teachers f their support")
+app.get("/user",(req,res)=>{
+    res.send("User information");
 });
+app.post("/user",(req,res)=>{
+    res.send("User created");
+});
+
