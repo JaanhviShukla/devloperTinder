d64b49e2abfd0246c0937d518cb32ea72695fcc6
JaanhviShukla <jaanhvishukla11@gmail.com>
2025-09-06 12:22:41 +0530

    feed get/api of devtinder

--- src/app.js
+++ src/app.js
@@ -12,6 +12,33 @@ connectDB.then(()=>{
 });
 const User=require("./models/user");
 app.use(express.json());
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    try{
+        const users=await User.find({emailId:userEmail});
+        if(users.length===0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);}
+            }
+            catch(err){
+                res.status(400).send("something went wrong");
+            }        
+    }
+);
+
+// FEED API to find all the users from database
+app.get("/feed",async(req,res)=>{
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+
 app.post("/signup",async(req,res)=>{
     console.log(req.body);
 
